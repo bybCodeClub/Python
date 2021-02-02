5d53c08c7432ff1279af8f8ef1b012666ae79601
the-chosen-3rd <42759948+the-chosen-3rd@users.noreply.github.com>
2021-02-02 02:50:33 -0800

    file added
    The first python problem from the book
    Python for everybody by Charles Severance

---
+++ problem1.py
@@ -0,0 +1,19 @@
+# Chapter 2  :  problem 2
+
+# write a program that uses input to ask a user for their and then welcomes them.
+
+name = input("What is you name ? ")
+
+print(f"hello {name}")
+
+
+
+
+
+
+
+# logic
+# Step 1: first we ask the user for their name,  then store that name in a variable
+# step 2: Then we print out their name using an f string
+
+# not if your curious about f-strings learn more >>>  https://www.youtube.com/watch?v=0fq69_9iEqc   
