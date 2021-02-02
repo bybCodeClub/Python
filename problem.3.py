c4e183aef6e74e3261c16ec76e243ea96d1205a3
the-chosen-3rd <42759948+the-chosen-3rd@users.noreply.github.com>
2021-02-02 02:11:41 -0800

    Update problem.3.py
    Logic added

--- problem.3.py
+++ problem.3.py
@@ -1,7 +1,16 @@
+# build a simple Celsius into fahrenheit converter.
 
-
-# here is a simple conversion from celsius to
 
 celsius = int(input("What is the temperature in Celicus? "))
-con = (celsius * 9/5) + 32
-print(f"That is {con} in fahrenheit ")
+convertor = (celsius * 9/5) + 32
+print(f"That is {convertor} in fahrenheit ")
+
+
+
+# Logic behind it 
+# step 1 you need to have a numbe in celsius, that can be stored in a variable.  
+# Then make sure that variable can take inputs and convert them into interigers (see line 4)
+
+# Step 2: use the celsius to fahrenheit equation on the celsius variable.
+
+# Step 3: Print the results   
