1d8a226236bcc10353cbae4fe35e5105e11e4825
the-chosen-3rd <42759948+the-chosen-3rd@users.noreply.github.com>
2021-02-02 03:19:56 -0800

    added file
    problem 4

---
+++ problem4.py
@@ -0,0 +1,28 @@
+# Chaper 2 : problem 4
+# python for everybody
+
+# Assume that we execute the following assignment statements. 
+
+width  = 17 
+height = 12.0
+
+# for each of the following expressions, write the value of the expressions and type (of the value of the expressions)
+
+# 1. width//2
+
+# 2. width/2.0
+
+# 3. height/3
+
+
+print(width//2)
+
+print(width/2.0)
+
+print(height/3)
+
+
+# answer 1:   = 8
+# answer 2:   = 8.5
+# answer 3:   = 4.0
+
