4077ad49016b7b796d1af06e7a1335b408723b7f
the-chosen-3rd <hamilton.michael13@yahoo.com>
2021-02-13 10:16:20 -0800

    puting chapter problems into folders

--- chapter 5/problem11.py
+++ chapter 5/problem11.py
@@ -1,3 +1,5 @@
+
+
 # write a program which repeatedly reads numbers until user enters "done". 
 # once done is entered, print out the total, count and average of the numbers. If the user enter anything other then a number,
 # detect their mistake using try & except and print an error massage and skip to the next number.
