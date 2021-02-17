50e2b9ae0da12ace389e854e4dc8f61f3667520e
Chosen 3rd <42759948+the-chosen-3rd@users.noreply.github.com>
2021-02-16 23:48:45 -0800

    Update chapter6_ex5.py

--- chapter6_ex5.py
+++ chapter6_ex5.py
@@ -1,4 +1,3 @@
-#
 # problem 12
 # Chapter 6 exersice 5 
 
@@ -7,13 +6,13 @@
 # then use the float function to convert the extracted string into a float number.
 
 
-original_string = "X-DSPAM-Confidence:0.8475"
-find_colen = original_string.find(":")
+original_string = "X-DSPAM-Confidence:0.8475"            
+find_colen = original_string.find(":")            # this gives us the index location of the colen within this string which is at 18 
 print(find_colen)
 
-part_to_convert = float(original_string[19:])
+part_to_convert = float(original_string[19:])     # Here we are taking everything after the colen and converting it into a float
 print(part_to_convert)
 
-checkingWork = (part_to_convert + 99) # which should equal 99.8475
+checkingWork = (part_to_convert + 99) # This is just to make sure that we can add/ subtract from this variable, It should equal 99.8475
 print(checkingWork) 
 
